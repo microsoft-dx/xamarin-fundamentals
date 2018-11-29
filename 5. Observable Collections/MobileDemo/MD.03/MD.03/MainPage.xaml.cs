6ab8896aa442fb6a90a908167e1919719009688c
Alexandru Petrescu <Alexandru.Petrescu@studentpartner.com>
2018-11-29 20:23:45 +0200

    Updated Demo for Observable Collections

--- 5. Observable Collections/MobileDemo/MD.03/MD.03/MainPage.xaml.cs
+++ 5. Observable Collections/MobileDemo/MD.03/MD.03/MainPage.xaml.cs
@@ -8,6 +8,7 @@ namespace MD._03
     public partial class MainPage : ContentPage
     {
         public ObservableCollection<ShoppingItem> ShoppingItems { get; set; }
+        private int itemCount;
 
         //public List<ShoppingItem> ShoppingItemsList { get; set; }
 
@@ -15,40 +16,33 @@ namespace MD._03
         {
             InitializeComponent();
             InitializeItems();
-
             ContentListView.ItemsSource = ShoppingItems;
-            //contentListView.ItemsSource = ShoppingItemsList;
         }
 
         private void InitializeItems()
         {
             //TODO This will be populated from the database
-            var contentForCollection = new List<ShoppingItem>
-            {
-                new ShoppingItem { ItemId = 1, ItemName = "item1"},
-                new ShoppingItem { ItemId = 2, ItemName = "item2"},
-                new ShoppingItem { ItemId = 3, ItemName = "item3"},
-                new ShoppingItem { ItemId = 4, ItemName = "item4"}
-            };
+            List<ShoppingItem> contentForCollection = new List<ShoppingItem>();
 
-            //var contentForList = new List<ShoppingItem>
-            //{
-            //    new ShoppingItem { ItemId = 1, ItemName = "item1"},
-            //    new ShoppingItem { ItemId = 2, ItemName = "item2"},
-            //    new ShoppingItem { ItemId = 3, ItemName = "item3"},
-            //    new ShoppingItem { ItemId = 4, ItemName = "item4"}
-            //};
+            for (int i = 0; i < 5; i++)
+            {
+                contentForCollection.Add(new ShoppingItem(itemCount, $"Item{itemCount++:00}"));
+            }
 
             this.ShoppingItems = new ObservableCollection<ShoppingItem>(contentForCollection);
-            //this.ShoppingItemsList = contentForList;
         }
 
-        private void ContentListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        private void AddNewItem(object sender, System.EventArgs e)
         {
-            var item = (sender as ListView).SelectedItem as ShoppingItem;
+            this.ShoppingItems.Add(new ShoppingItem(itemCount, $"Item{itemCount++:00}"));
+        }
 
-            this.ShoppingItems.RemoveAt(this.ShoppingItems.IndexOf(item));
-            //this.ShoppingItemsList.RemoveAt(this.ShoppingItemsList.IndexOf(item));
+        private void RemoveLastItem(object sender, System.EventArgs e)
+        {
+            if (itemCount > 0)
+            {
+                this.ShoppingItems.RemoveAt(--itemCount);
+            }
         }
     }
 }
--- 5. Observable Collections/MobileDemo/MD.03/MD.03/Models/ShoppingItem.cs
+++ 5. Observable Collections/MobileDemo/MD.03/MD.03/Models/ShoppingItem.cs
@@ -2,6 +2,12 @@
 {
     public class ShoppingItem
     {
+        public ShoppingItem(int givenID, string givenName)
+        {
+            this.ItemId = givenID;
+            this.ItemName = givenName;
+        }
+
         public int ItemId { get; set; }
 
         public string ItemName { get; set; }
